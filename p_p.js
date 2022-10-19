fe2b6ffbffd0074a42c9a18a41d8cf15e939fdc8
sourav samanta <saifactplanet@gmail.com>
2022-10-20 00:46:45 +0530

    code check

--- p_p.js
+++ p_p.js
@@ -1,28 +1,32 @@
-//checking for prime number
+	//checking for prime number
 
-let num = 13
-let count = 0
+	let num = 8;
+	let count = 0;
 
-for(let i =1; i<=num; i++){
-    if(num%i==0)
-    count++
-} if(count==2){
-     //  console.log("no")
-    }else {
-        //console.log("yes")
-    }
+	for(let i =1; i<=num; i++){
+   	 if(num%i==0){
+   	 count++
+    	}
+	} 
+	if(count==2){
+       	console.log("Yes")
+   	 }
+	else {
+      	  console.log("No")
+   	 }
 
-    //checking for palindrome
 
-    let str = "madam"
-    let N = 5
-    let bag = ""
-    for(let i = N-1; i>=0;  i--){
-        bag=(str[i])
-    }//console.log(bag)
+    	//checking for palindrome
+
+    	let str = "madam"
+    	let N = 5
+    	let bag = ""
+    	for(let i = N-1; i>=0;  i--){
+        	bag+=(str[i])
+    	}
     
-    if(bag==str){
-        console.log("Yes")
-    }else{
-        console.log("No")
-    }
+   	 if(bag==str){
+      	  console.log("Yes")
+    	}else{
+     	   console.log("No")
+    	}
